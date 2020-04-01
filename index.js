dfa864137ba166bbb3c2c8565ddea16bcf3c4407
github username <meliajm@gmail.com>
2020-04-01 11:53:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity='go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visual_flair='*') {
+    return function(adj="special") {
+        return `You are ${visual_flair}${adj}${visual_flair}!`
+      }
+}
+
+const Calculator = {
+}
+
+Calculator.add = function(n, m) {
+    return n + m
+}
+
+Calculator.subtract = function(n, m) {
+    return n - m
+}
+
+Calculator.multiply = function(n, m) {
+    return n * m
+}
+
+Calculator.divide = function(n, m) {
+    return n / m
+}
+
+function actionApplyer(startingInteger, arrayOfFunctions) {
+    if (arrayOfFunctions = []){
+        return startingInteger
+    } else {
+        return startingInteger
+    }
+
+
+}
